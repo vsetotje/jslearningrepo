472eed920842c07c4e22b368cdc6d5b00a41cb8d
vsetotje <vsetotje@gmail.com>
2021-08-20 16:51:05 +0300

    latest

--- extention project/index.js
+++ extention project/index.js
@@ -1,3 +1,25 @@
-function saveLead(){
-    console.log("Btn clicked")
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+    renderLeads()
+    inputEl.value= ""
+})
+
+function renderLeads(){
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>`
+    console.log(listItems)
+}
+ulEl.innerHTML = listItems
 }
